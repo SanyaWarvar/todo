dc2a3788730953a584dfa588f7de49d424b66f94
Gladin Danill <danyagladin@gmail.com>
2024-08-30 18:18:47 +0700

    add sign in

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -27,5 +27,21 @@ func (h *Handler) signUp(c *gin.Context) {
 }
 
 func (h *Handler) signIn(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,23 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
 	"github.com/SanyaWarvar/todo-app"
 	"github.com/SanyaWarvar/todo-app/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "pashatexnik"
+const (
+	salt       = "pashatexnik"
+	tokenTTL   = time.Hour * 24
+	signingKey = "radiance"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	Userid int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -26,6 +37,25 @@ func (s *AuthService) CreateUser(user todo.User) (int, error) {
 func (s *AuthService) generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
-
 	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		&tokenClaims{
+			jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+				IssuedAt:  time.Now().Unix(),
+			},
+			user.Id,
+		},
+	)
+
+	return token.SignedString([]byte(signingKey))
 }
