247c10abf92bcecc779e9c2f2ad06f8d165cf6c3
Gladin Danill <danyagladin@gmail.com>
2024-08-30 16:15:03 +0700

    base architecture complete

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,54 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/SanyaWarvar/todo-app"
 	"github.com/SanyaWarvar/todo-app/pkg/handler"
 	"github.com/SanyaWarvar/todo-app/pkg/repository"
 	"github.com/SanyaWarvar/todo-app/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.Repository{}
-	services := service.NewService(&repos)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error while initializing config: %s", err.Error())
+
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error while load dotenv: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("Error while create connection to db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatal(err)
+
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("Error while running server: %s", err.Error())
 	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
--- user.go
+++ user.go
@@ -2,6 +2,6 @@ package todo
 
 type User struct {
 	Id       int    `json:"-"`
-	Name     string `json:"name"`
+	Username string `json:"username"`
 	Password string `json:"password"`
 }
